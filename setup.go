35e076cd73ffa7370fc445cd8a30a0bc811e0a3b
root <root@beaglebone.(none)>
2014-07-13 04:10:45 +0000

    Add read pin functionality to setup.go

--- setup.go
+++ setup.go
@@ -12,28 +12,47 @@ func check(e error) {
     }
 }
 
-func readPin(pin int) int {
-    ainName := fmt.Sprintf("/sys/devices/ocp.3/helper.15/AIN%d", pin)
-    ainInput, err := os.Open(ainName)
+func readPin(bank int) int {
+//    time.Sleep(100 * time.Millisecond)
+    ainName := fmt.Sprintf("/sys/devices/ocp.3/helper.15/AIN%d", bank)
+    ainInput, err := os.OpenFile(ainName, os.O_RDONLY,0444)
     check(err)
 	text := make([]byte, 10)
 	count, err := ainInput.Read(text)
-	check(err)
+	time.Sleep(50 * time.Millisecond)
+	count, err = ainInput.Read(text)
+	if (err != nil) {
+		count, err = ainInput.Read(text)
+		check(err)
+	}
 	s := string(text[0:count-1])
 	val,err := strconv.Atoi(s)
 	check(err)
     err = ainInput.Close();
     check(err)
-    return val
+//    fmt.Printf("%d ", val)
+//	time.Sleep(10 * time.Millisecond)
+	return val
 }
 
-var values = []int{0,0,0,0,0,0,0}
-
-func ReadAIO() []int {
-	for p:=0; p<7; p++ {
-		values[p] = readPin(p)
+func readGPIO(pin int) int {
+    gpioName := fmt.Sprintf("/sys/class/gpio/gpio%d/value", pin)
+    gpioInput, err := os.Open(gpioName)
+    check(err)
+	text := make([]byte, 10)
+	count, err := gpioInput.Read(text)
+	if (err != nil) {
+		count, err = gpioInput.Read(text)
+		check(err)
 	}
-    return values
+	_ = count
+	s := string(text[0:count-1])
+	val,err := strconv.Atoi(s)
+	check(err)
+    err = gpioInput.Close();
+    check(err)
+//    fmt.Printf("%d ", val)
+    return val	
 }
 
 func setGPIO(pin int, value int) {
@@ -43,33 +62,64 @@ func setGPIO(pin int, value int) {
     s := fmt.Sprintf("%d",value)
 	count, err := gpioOutput.Write( []byte(s))
 	check(err)
-	count++
+	_ = count
+//	fmt.Printf("wrote: %d", count)
     err = gpioOutput.Close();
     check(err)
     return
 }
 
 func setChannel (channel int) {
+//	fmt.Printf("Chan:%d",channel)
 	setGPIO(23, channel&1)
-	setGPIO(47, channel&2)
-	setGPIO(27, channel&4)
-	setGPIO(22, channel&8)
+	setGPIO(47, channel&2/2)
+	setGPIO(27, channel&4/4)
+	setGPIO(22, channel&8/8)
+ //   fmt.Printf("%d -> %d %d %d %d ", channel, channel&1,channel&2/2,channel&4/4,channel&8/8)
+ //   val23 := readGPIO(23)
+ //   val47 := readGPIO(47)
+ //   val27 := readGPIO(27)
+ //   val22 := readGPIO(22)
+//    fmt.Printf("(%d %d %d %d)\n ", val23,val47,val27,val22)
 }
 
 func readSensor(bank int) []int {
 	values := make([]int,16,16)
 	for i:=0; i<16; i++{
 		setChannel(i)
-		values[i] = readPin(bank)
+		pValue := readPin(bank)
+		pValue = readPin(bank)
+		pValue = readPin(bank)
+		pValue = readPin(bank)
+		pValue = readPin(bank)
+		pValue = readPin(bank)		
+		pValue = readPin(bank)		
+		pValue = readPin(bank)		
+		values[i] = pValue
+//   		fmt.Printf(" Value: %d\n", pValue)		
 	}
 	return values
 }
 
+func printSensor(bank int, values []int){
+	fmt.Printf("%d ", bank)
+	for i:=0; i<16; i++ {
+		if values[i] > 300 {
+			fmt.Printf("X ")
+		} else {
+			fmt.Printf("- ")
+		}
+	}
+	fmt.Printf("\n")
+}
+
 func main() {
-	setChannel(0);
     for i:=0; i<10000000; i++ {
-    	values := readSensor(0)
-    	fmt.Printf("%.4d\n", values)
-		time.Sleep(1)
+    	values0 := readSensor(0)
+    	values1 := readSensor(1)
+    	printSensor(0,values0)
+    	printSensor(1,values1)
+    	fmt.Printf("\n")
+//		time.Sleep(1000 * time.Millisecond)
 	}
 }
