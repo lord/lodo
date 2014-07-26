3af3a3af6d71d7e34f2debfd9bc4603b3b753e61
James Lord <james@lord-family.net>
2014-07-26 04:18:50 +0000

    changed dance to support multiple waveforms

--- color.go
+++ color.go
@@ -1,5 +1,8 @@
 package main
 
+import "time"
+import "math/rand"
+
 type Color struct {
 	R, G, B int
 }
@@ -10,4 +13,12 @@ func MakeColor(r, g, b int) Color {
 		G: g,
 		B: b,
 	}
+}
+
+func randomColor() Color {
+	t := time.Now().UnixNano()
+	r := rand.New(rand.NewSource(t))
+	g := rand.New(rand.NewSource(t+1))
+	b := rand.New(rand.NewSource(t+2))
+	return MakeColor(r.Intn(255), g.Intn(255), b.Intn(255))
 }
--- dance.go
+++ dance.go
@@ -2,32 +2,76 @@ package main
 
 //import "errors"
 import "time"
+import "math"
+//import "fmt"
 
-type dwave struct 
+type dwave struct {
+	x      float32
+	y      float32
+	dist   float32
+	start  time.Time
+	color  Color
+	active int
+}
+
+var dw [30] dwave
 
 func process_dance(brd Board, t time.Duration) error {
+	brd.DrawAll(0,0,0)
 	for row:=0; row< 5; row++ {
 		for col:=0; col<4; col++ {
 			state := brd.getBoardState(row,col)
-			if (state == down || state == pressed ) {
-				_ = brd.DrawSquare(col,row,255,255,255)
+			if (state == pressed ) {
+				color := randomColor()
+				_ = brd.DrawSquare(col,row,color.R,color.G,color.B)
+				addWave(col, row, color)
 			}
 		}
 	}
 	
-	offset := 0.0
-	for {
-		offset -= 0.1
-		for x := 0; x < w; x++ {
-			for y := 0; y < h; y++ {
-				dx := x - 10
-				dy := y - 10
-				dis := math.Sqrt(float64(dx*dx + dy*dy))
-				amt := int(math.Sin(dis+offset)*250.0 + 1.0)
-				board.DrawPixel(x, y, amt, 10, amt)
+	for i:=0; i<30; i++ {
+		if dw[i].active == 1 { 
+			wDist := float32(time.Since(dw[i].start)/100000000)
+			if (wDist > 25) {
+				dw[i].active = 0
+			} else {
+				for x := 0; x < brd.pixelW; x++ {
+					for y := 0; y < brd.pixelH; y++ {
+						dx := float32(x) - dw[i].x
+						dy := float32(y) - dw[i].y
+						dis := float32(math.Sqrt(float64(dx*dx + dy*dy)))
+						decay := 1.0-wDist/25
+						if dis > wDist-1.5 && dis<wDist+1.5 {
+							amt := (dis-wDist)/1.5
+							if amt>0 { 
+									amt = (1-amt)*decay
+								} else { 
+									amt = (1+amt)*decay
+								}
+							
+							brd.DrawPixel(x, y, int(amt*float32(dw[i].color.R)), 
+								int(amt*float32(dw[i].color.G)), 
+								int(amt*float32(dw[i].color.B)))
+						}
+					}
+				}
 			}
 		}
 	}
-
 	return nil
 }
+
+func addWave(col int, row int, color Color) {
+	for i:=0; i<30; i++ {
+		if dw[i].active != 1 {
+			dw[i].x = float32(col)*5+2.5
+			dw[i].y = float32(row)*5+2.5
+			dw[i].active = 1
+			dw[i].color = color
+			dw[i].dist = 0
+			dw[i].start = time.Now()
+			break;
+		}
+	}
+}
+
