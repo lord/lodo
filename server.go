76ef01693266237078e5a71956c30748fcf82788
Robert Lord <robert@lord.io>
2014-07-28 05:56:06 +0000

    Add clear command to server

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import "strconv"
 
 func RunServer(brd *Board) {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		q := req.URL.Query()
 		if len(q["r"]) == 0 || len(q["g"]) == 0 || len(q["b"]) == 0 || len(q["x"]) == 0 || len(q["y"]) == 0 {
 			fmt.Fprint(w, "Missing values")
@@ -17,6 +18,11 @@ func RunServer(brd *Board) {
 		x, _ := strconv.ParseInt(q["x"][0], 10, 0)
 		y, _ := strconv.ParseInt(q["y"][0], 10, 0)
 		brd.DrawPixel(int(x), int(y), MakeColor(int(r), int(g), int(b)))
+		brd.Save()
+	})
+	http.HandleFunc("/clear", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		brd.DrawAll(MakeColor(0, 0, 0))
 		brd.Save()
 	})
 	http.ListenAndServe(":8070", nil)
