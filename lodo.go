93907017b6518bd072488e014337b25bf18378f3
Robert Lord <robert@lord.io>
2014-07-28 05:45:48 +0000

    Add server for drawing tests

--- lodo.go
+++ lodo.go
@@ -23,16 +23,18 @@ func main() {
 		return
 	}
 
-	for {
-		board.DrawAll(MakeColor(0, 0, 0))
-		err = board.DrawRectOutline(9, 9, 15, 15, MakeColor(255, 0, 0))
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
-		board.DrawLine(10, 0, 10, 19, MakeColor(200, 200, 200))
-		board.Save()
-	}
+	RunServer(&board)
+
+	// for {
+	// 	board.DrawAll(MakeColor(0, 0, 0))
+	// 	err = board.DrawRectOutline(9, 9, 15, 15, MakeColor(255, 0, 0))
+	// 	if err != nil {
+	// 		fmt.Println("Error:", err)
+	// 		return
+	// 	}
+	// 	board.DrawLine(10, 0, 10, 19, MakeColor(200, 200, 200))
+	// 	board.Save()
+	// }
 
 	// poll := make(chan string)
 	// // go board.pollSensors(poll)
