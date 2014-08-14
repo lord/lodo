871caa79b58063936e0ff6302409708666eae4b9
Robert Lord <robert@lord.io>
2014-08-14 07:53:21 +0000

    Add player movement

--- maze/maze.go
+++ maze/maze.go
@@ -4,28 +4,41 @@ import "github.com/lord/lodo/core"
 import "time"
 
 type Game struct {
-	objects *[]GameObject
+	objects []GameObject
+	board   *core.Board
+}
+
+func (game *Game) CheckPressed(x, y int) bool {
+	if x < 0 || x > 4 || y < 0 || y > 5 {
+		return false
+	}
+	return game.board.CheckPressed(x, y)
 }
 
 type GameObject interface {
-	step(*Game)
-	draw(*core.Board)
+	Step(*Game)
+	Draw(*core.Board)
 }
+
+const xSquares = 5
+const ySquares = 6
 
 func Run(board *core.Board) {
 	black := core.MakeColor(0, 0, 0)
 	ticker := time.Tick(33 * time.Millisecond)
 	game := &Game{
-		objects: &[]GameObject{},
+		objects: []GameObject{},
+		board:   board,
 	}
+	game.objects = append(game.objects, &Player{x: 0, y: 0})
 	for _ = range ticker {
 		board.RefreshSensors()
 		board.DrawAll(black)
-		for _, obj := range *game.objects {
-			obj.step(game)
+		for _, obj := range game.objects {
+			obj.Step(game)
 		}
-		for _, obj := range *game.objects {
-			obj.draw(board)
+		for _, obj := range game.objects {
+			obj.Draw(board)
 		}
 		board.Save()
 	}
---
+++ maze/player.go
@@ -0,0 +1,53 @@
+package maze
+
+import "github.com/lord/lodo/core"
+
+type Player struct {
+	x, y         int
+	drawX, drawY int
+}
+
+var color = core.MakeColor(0, 10, 0)
+
+func MakePlayer(x, y int) *Player {
+	return &Player{
+		x:     x,
+		y:     y,
+		drawX: x * 7,
+		drawY: y * 7,
+	}
+}
+
+func (player *Player) Step(game *Game) {
+	if game.CheckPressed(player.x+1, player.y) {
+		player.x += 1
+	} else if game.CheckPressed(player.x, player.y+1) {
+		player.y += 1
+	} else if game.CheckPressed(player.x-1, player.y) {
+		player.x -= 1
+	} else if game.CheckPressed(player.x, player.y-1) {
+		player.y -= 1
+	}
+
+	targetX, targetY := game.board.GetSquare(player.x, player.y)
+	if targetX > player.drawX {
+		player.drawX++
+	}
+	if targetX < player.drawX {
+		player.drawX--
+	}
+	if targetY > player.drawY {
+		player.drawY++
+	}
+	if targetY < player.drawY {
+		player.drawY--
+	}
+}
+
+func (player *Player) Draw(board *core.Board) {
+	board.DrawPixel(player.drawX+3, player.drawY+3, color)
+	board.DrawPixel(player.drawX+4, player.drawY+3, color)
+	board.DrawPixel(player.drawX+3, player.drawY+4, color)
+	board.DrawPixel(player.drawX+3, player.drawY+2, color)
+	board.DrawPixel(player.drawX+2, player.drawY+3, color)
+}
