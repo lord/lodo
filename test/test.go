f8b3b20700101f049e41f42833e209d2e684fb82
Robert Lord <robert@lord.io>
2014-08-14 03:59:54 +0000

    Gofmt test

--- test/test.go
+++ test/test.go
@@ -11,15 +11,15 @@ func Run(board *core.Board) {
 	board.RefreshSensors()
 	blue := core.MakeColor(0, 0, 2)
 	white := core.MakeColor(10, 10, 10)
-//	red := core.MakeColor(0, 31, 0)
+	//	red := core.MakeColor(0, 31, 0)
 	for {
 		board.RefreshSensors()
 		for x := 0; x < 5; x++ {
-			for y:= 0; y<6; y++ {
+			for y := 0; y < 6; y++ {
 				if board.CheckDown(x, y) {
-					board.FillSquare(x,y,white)	
+					board.FillSquare(x, y, white)
 				} else {
-					board.FillSquare(x,y,blue)
+					board.FillSquare(x, y, blue)
 				}
 			}
 		}
